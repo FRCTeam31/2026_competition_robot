4d867a9097e923f13bdb25ba51ccd4268c75a95e
Hudson <arrowvark.gaming@gmail.com>
2026-01-17 13:33:28 -0600

    Finalized hopper sim implementation

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 46;
-  public static final String GIT_SHA = "adadc81848d247c0461a87bd3f24c0a9def37a3d";
-  public static final String GIT_DATE = "2026-01-17 10:52:42 CST";
+  public static final int GIT_REVISION = 47;
+  public static final String GIT_SHA = "6cec45d1099d4b2db6acfce7172738640c6743f1";
+  public static final String GIT_DATE = "2026-01-17 11:46:32 CST";
   public static final String GIT_BRANCH = "subsystemPrototypes";
-  public static final String BUILD_DATE = "2026-01-17 10:53:12 CST";
-  public static final long BUILD_UNIX_TIME = 1768668792949L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-01-17 11:52:17 CST";
+  public static final long BUILD_UNIX_TIME = 1768672337744L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
