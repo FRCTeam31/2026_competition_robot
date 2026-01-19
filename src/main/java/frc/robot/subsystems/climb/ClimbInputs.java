c73ba44d7f77fe1aba1b6758414d580a71f82a41
Hudson <arrowvark.gaming@gmail.com>
2026-01-19 15:05:05 -0600

    Added support state to ClimbInputs

--- src/main/java/frc/robot/subsystems/climb/ClimbInputs.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbInputs.java
@@ -5,4 +5,5 @@ import org.littletonrobotics.junction.AutoLog;
 @AutoLog
 public class ClimbInputs {
     public Climb.ClimbState climbState;
+    public Climb.SupportState supportState;
 }
--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 56;
-  public static final String GIT_SHA = "27ed9cfd0b3615e3903dc2310cfc76d05cebb08e";
-  public static final String GIT_DATE = "2026-01-19 13:51:25 CST";
+  public static final int GIT_REVISION = 57;
+  public static final String GIT_SHA = "04044b9ede2f2b47c1a43b6642f7f617ec6728fb";
+  public static final String GIT_DATE = "2026-01-19 14:42:44 CST";
   public static final String GIT_BRANCH = "subsystemPrototypes";
-  public static final String BUILD_DATE = "2026-01-19 14:40:20 CST";
-  public static final long BUILD_UNIX_TIME = 1768855220211L;
+  public static final String BUILD_DATE = "2026-01-19 15:04:14 CST";
+  public static final long BUILD_UNIX_TIME = 1768856654635L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
