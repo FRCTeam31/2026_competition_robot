afeb33aa121d3728278a3cb5cb503202addfb49e
Mason H <35619771+zeroClearAmerican@users.noreply.github.com>
2026-01-21 05:52:09 +0000

    fix: update path for code-server in post-create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -11,7 +11,7 @@ ln -sf /opt/wpilib/2026 ~/wpilib/2026
 echo "Installing WPILib extensions..."
 for vsix in /opt/wpilib/2026/vsCodeExtensions/*.vsix; do
     echo "Installing $(basename "$vsix")..."
-    ~/.vscode-server/bin/*/bin/code-server --install-extension "$vsix" --force
+    ~/.vscode-remote/bin/*/bin/code-server --install-extension "$vsix" --force
 done
 
 echo "WPILib 2026 environment ready!"
