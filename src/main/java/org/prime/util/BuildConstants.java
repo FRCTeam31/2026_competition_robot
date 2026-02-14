12fe5c816a4d071d280daf011e48ae17e3b1a439
David Ibom <davidebubeuchenna252@jenksms.com>
2026-02-14 10:42:27 -0600

    Decouple setToTargetVector from MutVector and rename to calculateAimVector

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 86;
-  public static final String GIT_SHA = "9a967b1f43cf6055a1cc0654034f9a8baf0ae986";
-  public static final String GIT_DATE = "2026-02-12 19:48:51 CST";
-  public static final String GIT_BRANCH = "TurretAimTesting";
-  public static final String BUILD_DATE = "2026-02-13 17:57:46 CST";
-  public static final long BUILD_UNIX_TIME = 1771027066099L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 87;
+  public static final String GIT_SHA = "cd9a7773a2380e53763e340c7d8d2d23781fb71e";
+  public static final String GIT_DATE = "2026-02-13 18:13:03 CST";
+  public static final String GIT_BRANCH = "turretAimRevamp";
+  public static final String BUILD_DATE = "2026-02-14 10:36:38 CST";
+  public static final long BUILD_UNIX_TIME = 1771086998452L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
