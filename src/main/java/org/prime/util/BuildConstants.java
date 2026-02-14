f6dfa2238d0a9f2fdff4a6f97f4869bb0233d5d6
ArtsNCrafters <howtoliveinpain@gmail.com>
2026-02-14 14:23:54 -0600

    Update turret rotation handling and refine yaw control logic

--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 92;
-  public static final String GIT_SHA = "8fe9e5f9aabb662fa3cae9e552e8e1386b267e72";
-  public static final String GIT_DATE = "2026-02-14 13:40:28 CST";
+  public static final int GIT_REVISION = 95;
+  public static final String GIT_SHA = "c4d8fa23a5d2ff3c45b7c03e712f009849c957a0";
+  public static final String GIT_DATE = "2026-02-14 14:00:43 CST";
   public static final String GIT_BRANCH = "subsystemPrototypes";
-  public static final String BUILD_DATE = "2026-02-14 13:55:34 CST";
-  public static final long BUILD_UNIX_TIME = 1771098934850L;
+  public static final String BUILD_DATE = "2026-02-14 14:22:15 CST";
+  public static final long BUILD_UNIX_TIME = 1771100535796L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
