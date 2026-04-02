2db8d631609ea23eac6125f6ff4247c3dc9e7191
Hudson <arrowvark.gaming@gmail.com>
2026-04-02 16:31:01 -0500

    Update at 'Thu Apr 02 16:31:01 CDT 2026'

--- src/main/java/frc/robot/subsystems/turret/Turret.java
+++ src/main/java/frc/robot/subsystems/turret/Turret.java
@@ -308,8 +308,7 @@ public class Turret extends LoggedSubsystem {
 
         // Hood
         var pitch = _mutNominalTargetVector.getPitch();
-        // _turret.controlHood(Degrees.of(pitch));
-        _turret.controlHood(Degrees.of(TurretMap.HOOD_MIN_ANGLE_DEGREES));
+        _turret.controlHood(Degrees.of(pitch));
         _testtargetvelocity = _mutNominalTargetVector.getMagnitude();
         // Flywheel
         if (inputs.FiringState == FiringState.FIRING) {
--- src/main/java/org/prime/util/BuildConstants.java
+++ src/main/java/org/prime/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2026_competition_robot";
   public static final String VERSION = "0.1.0";
-  public static final int GIT_REVISION = 59;
-  public static final String GIT_SHA = "15ad84a0d5f0173ea7f14502d9b6f5860ccf82c1";
-  public static final String GIT_DATE = "2026-04-02 16:26:26 CDT";
+  public static final int GIT_REVISION = 60;
+  public static final String GIT_SHA = "e221f7c671e50810f80a57cad62a6e7dce0afc95";
+  public static final String GIT_DATE = "2026-04-02 16:27:27 CDT";
   public static final String GIT_BRANCH = "OKTU-autocommit";
-  public static final String BUILD_DATE = "2026-04-02 16:26:26 CDT";
-  public static final long BUILD_UNIX_TIME = 1775165186879L;
+  public static final String BUILD_DATE = "2026-04-02 16:27:28 CDT";
+  public static final long BUILD_UNIX_TIME = 1775165248048L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
