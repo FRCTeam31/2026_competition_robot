74588777e4d6b8ef0d2b6686a4283ede65b6fc9c
Mason H <35619771+zeroClearAmerican@users.noreply.github.com>
2026-04-03 11:12:01 -0500

    added phase 2 measurements for flyspeed

--- src/main/java/frc/robot/subsystems/turret/TurretMap.java
+++ src/main/java/frc/robot/subsystems/turret/TurretMap.java
@@ -88,24 +88,27 @@ public class TurretMap {
         public static double MAX_HOOD_MIN_DIST_METERS = Units.inchesToMeters(47.5);
         public static double MAX_HOOD_MAX_DIST_METERS = Units.inchesToMeters(140.5);
         public static final InterpolatingDoubleTreeMap TARGET_DIST_FLYSPEED_RPS_MAX_HOOD_MAP = InterpolatingDoubleTreeMap
-                        .ofEntries(
-                                        Map.entry(MAX_HOOD_MIN_DIST_METERS, RPM.of(2700).in(RotationsPerSecond)),
+                        .ofEntries(Map.entry(MAX_HOOD_MIN_DIST_METERS, RPM.of(2700).in(RotationsPerSecond)),
                                         Map.entry(Units.inchesToMeters(97.5), RPM.of(3200).in(RotationsPerSecond)),
                                         Map.entry(Units.inchesToMeters(109.5), RPM.of(3400).in(RotationsPerSecond)),
                                         Map.entry(MAX_HOOD_MAX_DIST_METERS, RPM.of(3600).in(RotationsPerSecond)));
 
-        public static double MIN_HOOD_MIN_DIST_METERS = 0;
-        public static double MIN_HOOD_MAX_DIST_METERS = 10;
+        public static double MIN_HOOD_MIN_DIST_METERS = Units.inchesToMeters(72);
+        public static double MIN_HOOD_MAX_DIST_METERS = Units.inchesToMeters(239);
         public static final InterpolatingDoubleTreeMap TARGET_DIST_FLYSPEED_RPS_MIN_HOOD_MAP = InterpolatingDoubleTreeMap
-                        .ofEntries();
+                        .ofEntries(Map.entry(MIN_HOOD_MIN_DIST_METERS, RPM.of(2000).in(RotationsPerSecond)),
+                                        Map.entry(Units.inchesToMeters(108), RPM.of(2600).in(RotationsPerSecond)),
+                                        Map.entry(Units.inchesToMeters(152), RPM.of(3100).in(RotationsPerSecond)),
+                                        Map.entry(Units.inchesToMeters(213), RPM.of(3700).in(RotationsPerSecond)),
+                                        Map.entry(MIN_HOOD_MAX_DIST_METERS, RPM.of(4200).in(RotationsPerSecond)));
 
         // ------------------------ Hood -----------------------------------
         public static final int HOOD_CAN_ID = 18;
         public static final boolean HOOD_INVERTED = false;
         public static final double HOOD_MAX_ANGLE_DEGREES = 35.1; // Hood fully retracted
-        // public static final double HOOD_MAX_ANGLE_DEGREES = 23.85; // Hood fully retracted
-        // public static final double HOOD_MIN_ANGLE_DEGREES = 12.6; // Hood fully extended
-        public static final double HOOD_MIN_ANGLE_DEGREES = HOOD_MAX_ANGLE_DEGREES; // Hood fully extended
+        public static final double HOOD_HALF_ANGLE_DEGREES = 23.85;
+        public static final double HOOD_MIN_ANGLE_DEGREES = 12.6; // Hood fully extended
+        // public static final double HOOD_MIN_ANGLE_DEGREES = HOOD_MAX_ANGLE_DEGREES; // Hood fully extended
         public static final double HOOD_ANGLE_RANGE_DEGREES = HOOD_MAX_ANGLE_DEGREES - HOOD_MIN_ANGLE_DEGREES;
         public static final ExtendedPIDConstants HOOD_PID = new ExtendedPIDConstants(0.1, 0, 0);
         public static final double HOOD_ON_TARGET_TOLERANCE_DEGREES = 1.0;
